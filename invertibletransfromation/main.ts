42167bfff1463ae86dcd8fe9ad7a3552abe70ec4
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-01-30 14:18:26 -0500

    - change the `Step` interface to an abstract class, becaus in reality, how else would we expect an object to hold onto its `lost` data, while also being available for being used by other functions. we can't, unless we use a generator function for that object's generation, so the class way of doing it is the way to go for reusibility with a new instance. - furthermore, it opens the possibility of creating a (static) variation between the generated instances, as it can be seen with `BinaryNumberStep`, which is a binary codec for many different kinds of numbers, which must be fixed during the construction of the instance. - implement a binary codecs (aka steps), for numbers and strings - also implement the forward method for an array of composite item binary step. (the length of the array should be provided as argument `args: { length: number }`)

--- invertibletransfromation/main.ts
+++ invertibletransfromation/main.ts
@@ -1,82 +1,148 @@
-import { decode_str, encode_str, pack, unpack,  } from "./deps.ts"
+import { decode_str, encode_str, pack, unpack, EmptyObj, decode_cstr, encode_cstr, NumericType, decode_number, encode_number, decode_number_array, NumericArrayType, encode_number_array } from "./deps.ts"
 
 /** a single step consists a froward transformation {@link FROM | from a type} to a {@link TO | different type},
  * and a backward transfromation that does the reverse.
 */
-// interface Step<FROM, TO, LOST = any> {
-/** the forward transformation function */
-// forward: (input: FROM) => TO
-/** the reverse transformation function */
-// backward: (input: TO) => FROM
-/** information lost in the forward transformation should be stored here.
- * the lost information may be needed for the backward transformation.
-*/
-// lost: LOST
-// }
-
-// I think Step's forward and backward methods should be static, since `lost` should be bound to the `input`
-
-
-/** a single step consists a froward transformation {@link FROM | from a type} to a {@link TO | different type},
- * and a backward transfromation that does the reverse.
-*/
-interface Step<FROM, TO, LOST = any> {
-	/** the forward transformation function */
-	forward: (input: FROM) => TO
-	/** the reverse transformation function */
-	backward: (input: TO) => FROM
+abstract class Step<FROM, TO, LOST = any> {
 	/** information lost in the forward transformation should be stored here.
 	 * the lost information may be needed for the backward transformation.
 	*/
-	lost: LOST
+	protected abstract lost: LOST
+
+	/** the forward transformation function */
+	abstract forward(input: FROM): TO
+
+	/** the reverse transformation function */
+	abstract backward(input: TO): FROM
 }
 
-type BinaryOutput<T> = {
-	/** decoded output value */
-	val: T
-	/** bytelength occupied by the value when it was decoded */
-	len: number
-}
-type BinaryInput<T, ARGS = any[]> = {
+
+interface BinaryInput<ARGS = any> {
 	/** input binary data */
 	bin: Uint8Array
 	/** byte offet */
 	pos: number
 	/** args */
-	args?: ARGS
+	args: ARGS
 }
-type BinaryStep<
-	T,
-	OUT extends BinaryOutput<T> = any,
-	IN extends BinaryInput<T> = any,
+interface BinaryOutput<T> {
+	/** decoded output value */
+	val: T
+	/** bytelength occupied by the value when it was decoded */
+	len: number
+}
+abstract class BinaryStep<
+	OUT,
+	ARGS = any,
 	LOST = any
-> = Step<IN, OUT, LOST>
-
-
-interface BinaryInput_String extends BinaryInput<string, [length: number]> {
-	args: [length: number,]
+> extends Step<BinaryInput<ARGS>, BinaryOutput<OUT>, LOST> {
+	abstract backward(input: Omit<BinaryOutput<OUT>, "len">): BinaryInput<ARGS>
 }
-class BinaryStringStep implements BinaryStep<
-	string,
-	BinaryOutput<string>,
-	BinaryInput_String
-> {
-	lost: undefined
-	forward(input: BinaryInput_String): BinaryOutput<string> {
+type LengthedArgs = { length: number }
+abstract class BinaryLengthedDataStep<OUT, LOST = any> extends BinaryStep<OUT, LengthedArgs, LOST> { }
+
+
+class BinaryStringStep extends BinaryLengthedDataStep<string> {
+	protected lost: undefined
+	forward(input: BinaryInput<LengthedArgs>): BinaryOutput<string> {
 		const
-			{ bin, pos, args: [str_lenth] } = input,
+			{ bin, pos, args: { length: str_lenth } } = input,
 			[str, bytelength] = decode_str(bin, pos, str_lenth)
 		return { val: str, len: bytelength }
 	}
-	backward(input: BinaryOutput<string>): BinaryInput_String {
+	backward(input: Omit<BinaryOutput<string>, "len">): BinaryInput<LengthedArgs> {
 		const
 			bin = encode_str(input.val),
 			str_lenth = bin.length
-		return { bin, pos: 0, args: [str_lenth,] }
+		return { bin, pos: 0, args: { length: str_lenth } }
+	}
+}
+
+class BinaryCStringStep extends BinaryStep<string, never> {
+	protected lost: undefined
+	forward(input: BinaryInput): BinaryOutput<string> {
+		const
+			{ bin, pos } = input,
+			[str, bytelength] = decode_cstr(bin, pos)
+		return { val: str, len: bytelength }
+	}
+	backward(input: Omit<BinaryOutput<string>, "len">): BinaryInput<never> {
+		const bin = encode_cstr(input.val)
+		return { bin, pos: 0 } as any
+	}
+}
+
+class BinaryNumberStep<ENCODING extends NumericType> extends BinaryStep<number, never> {
+	// TODO: later on, add support for the variable sized numeric types `"iv"` and `"uv"`
+	protected readonly kind: ENCODING
+	protected lost!: undefined
+	constructor(kind: ENCODING) {
+		super()
+		this.kind = kind
+	}
+	forward(input: BinaryInput<{ kind: NumericType }>): BinaryOutput<number> {
+		const
+			{ bin, pos } = input,
+			[num, bytelength] = decode_number(bin, pos, this.kind)
+		return { val: num, len: bytelength }
+	}
+	backward(input: Omit<BinaryOutput<number>, "len">): BinaryInput<never> {
+		const bin = encode_number(input.val, this.kind)
+		return { bin, pos: 0 } as any
+	}
+}
+
+class BinaryNumberArrayStep<ENCODING extends NumericType> extends BinaryLengthedDataStep<number[]> {
+	// TODO: later on, add support for the variable sized numeric types `"iv"` and `"uv"`
+	protected readonly kind: ENCODING
+	protected lost: undefined
+	constructor(kind: ENCODING) {
+		super()
+		this.kind = kind
+	}
+	forward(input: BinaryInput<LengthedArgs>): BinaryOutput<number[]> {
+		const
+			{ bin, pos, args: { length } } = input,
+			[arr, bytelength] = decode_number_array(bin, pos, this.kind + "[]" as NumericArrayType, length)
+		return { val: arr, len: bytelength }
+	}
+	backward(input: Omit<BinaryOutput<number[]>, "len">): BinaryInput<LengthedArgs> {
+		const
+			val = input.val,
+			arr_len = val.length,
+			bin = encode_number_array(val, this.kind + "[]" as NumericArrayType)
+		return { bin, pos: 0, args: { length: arr_len } }
+	}
+}
+
+class BinaryArrayStep<OUT_ITEM, ITEM_STEP extends typeof BinaryStep<OUT_ITEM, never>> extends BinaryLengthedDataStep<OUT_ITEM[]> {
+	protected readonly base_class: ITEM_STEP
+	protected lost: any
+	constructor(base_class: ITEM_STEP) {
+		super()
+		this.base_class = base_class
+	}
+	forward(input: BinaryInput<LengthedArgs>): BinaryOutput<OUT_ITEM[]> {
+		const
+			{ bin, pos, args: { length } } = input,
+			base_class = this.base_class,
+			out_arr: OUT_ITEM[] = []
+		for (const iterator of object) {
+			base_class.forward({ bin, pos } as any)
+		}
+		[arr, bytelength] = decode_number_array(bin, pos, this.kind + "[]" as NumericArrayType, length)
+		return { val: arr, len: bytelength }
+
+		throw new Error("Method not implemented.")
+	}
+	backward(input: Omit<BinaryOutput<OUT_ITEM[]>, "len">): BinaryInput<LengthedArgs> {
+
+
+		throw new Error("Method not implemented.")
 	}
 }
 
 
+const b = new BinaryStringStep()
+b.backward({ val: "aaaa" })
 
-
-const a = unpack("bool", Uint8Array.of(), 8)[0]
