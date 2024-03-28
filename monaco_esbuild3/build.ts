0090b8c35e2b691b829e4f4675be56d28cd40e90
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-03-28 00:30:05 -0400

    write an esbuild plugin `iifePlugin` to convert selected output files from `"esm"` format to `"iife"`, in the very end of compilation.

--- monaco_esbuild3/build.ts
+++ monaco_esbuild3/build.ts
@@ -1,6 +1,8 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.20.1/mod.js"
 import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.9.0"
 import { cssPlugin } from "jsr:@oazmi/esbuild-plugin-css@0.1.1"
+import { ensureFile } from "jsr:@std/fs"
+import { extname } from "jsr:@std/path"
 // import { cssPlugin } from "file:///D:/My works/2024/esbuild-plugin-css/src/mod.ts"
 
 // NOTICE: notice that we are effectively wanting to compile our worker files and our `editor_base` together, with the only difference being that
@@ -18,8 +20,73 @@ import { cssPlugin } from "jsr:@oazmi/esbuild-plugin-css@0.1.1"
 // resulting transformed js contnets with the js loader (during the build.onLoad step).
 
 
+const iifePlugin = ({ filters }: {
+	filters: Array<RegExp>
+}): esbuild.Plugin => {
+	return {
+		name: "iife-plugin",
+		setup: (build) => {
+			if (build.initialOptions.splitting) {
+				console.warn(
+					"[iife-plugin]: WARNING: splitting will probably lead to faulty code!",
+					"\n\tthis is because esbuild replaces static `import` with `require(...)` instead of dynamic `await import(...)`",
+					"\n\tand `require` is not available anywhere besides Node"
+				)
+			}
+
+			build.onEnd(async (result: esbuild.BuildResult) => {
+				const
+					files_to_preserve: esbuild.OutputFile[] = [],
+					files_to_convert_to_iife = result.outputFiles!.filter((output_file) => {
+						const
+							{ path, hash } = output_file,
+							ext = extname(path),
+							hashless_path = path.replace(RegExp("-" + hash + ext + "$"), ext)
+						// console.log(path, hash, ext, hashless_path)
+						// NOTICE: the `hash` is actually different from the optional hash applied to the path names, unlike what I thought.
+						for (const filter of filters) {
+							if (filter.test(hashless_path)) {
+								return true
+							}
+						}
+						files_to_preserve.push(output_file)
+						return false
+					}),
+					iife_files = await Promise.all(files_to_convert_to_iife.map(async (output_file) => {
+						const { path, hash, contents } = output_file
+						let { code } = await build.esbuild.transform(contents, {
+							format: "iife",
+							platform: "browser",
+							// do not treeshake, as there is very likely going to be code that relies on side-effects when dynamic imports are used.
+							treeShaking: false,
+						})
+						if (build.initialOptions.splitting) {
+							// if splitting was enabled, our static imports will have unavoidably transformed into `require(...)`.
+							// thus we run a silly regex to replace all `require(...)` with `await import(...)`.
+							// additionally, we transform the top level iife function into async if it is not already an async function.
+							code = code
+								.replaceAll(/(?<![a-zA-Z0-9_$])require\(/g, "await import(")
+								.replace(/^\(\s*\(/, "(async (")
+						}
+						return {
+							path,
+							hash,
+							contents: new TextEncoder().encode(code),
+							text: code
+						} as esbuild.OutputFile
+					}))
+				result.outputFiles!.splice(0)
+				result.outputFiles!.push(...files_to_preserve, ...iife_files)
+				return
+			})
+		}
+	}
+}
+
+
 const css_mode: "bundle" | "inject" = "bundle"
-const write = true
+// write MUST BE FALSE for outputFiles to exist, and for the plugin to work
+const write: boolean = false
 
 const workers = {
 	editor: "npm:monaco-editor/esm/vs/editor/editor.worker.js",
@@ -31,8 +98,9 @@ const workers = {
 const editor_base = "npm:monaco-editor/esm/vs/editor/editor.main.js"
 
 const result1 = await esbuild.build({
-	plugins: [cssPlugin({ mode: css_mode }), ...denoPlugins()],
+	plugins: [cssPlugin({ mode: css_mode }), ...denoPlugins(), iifePlugin({ filters: [/\.worker\.js$/,] })],
 	entryPoints: [
+		editor_base,
 		workers.editor,
 		workers.json,
 		workers.css,
@@ -43,31 +111,24 @@ const result1 = await esbuild.build({
 	// the "npm:" (with the colon) transforms into "npm_"
 	outbase: "npm_monaco-editor/esm/vs/",
 	// workers should not export anything. they behave like iife more or less
-	format: "iife",
-	minify: false,
-	treeShaking: true,
-	bundle: true,
-	write
-})
-
-const result2 = await esbuild.build({
-	plugins: [cssPlugin({ mode: css_mode }), ...denoPlugins()],
-	entryPoints: [
-		editor_base,
-	],
-	outdir: "./dist/",
-	// the "npm:" (with the colon) transforms into "npm_"
-	outbase: "npm_monaco-editor/esm/vs/",
-	// this is what we will import into our main script
 	format: "esm",
 	minify: false,
 	treeShaking: true,
 	bundle: true,
-	write
+	// the `iifePlugin` does not work with splitting, because static `import` gets transformed into `require(...)` instead of dynamic `await import(...)`
+	// splitting: true,
+	platform: "browser",
+	write,
 })
 
-if(write === false) {
+await Promise.all(result1.outputFiles.map(async (output_file) => {
+	const { path, contents } = output_file
+	await ensureFile(path)
+	await Deno.writeFile(path, contents)
+}))
+
+
+if (write === false) {
 	console.log(result1.outputFiles.map((v) => v.path))
-	console.log(result2.outputFiles.map((v) => v.path))
 }
 
