587d7d97fc530e7b4593ce0db1570264a3dd28e8
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2025-03-12 07:17:51 -0400

    create a new project in `/monaco_esbuild4/` for bundling the `monaco-editor` using esbuild in nodejs (not made to be compatible with deno's official esbuild plugin, unless a css-plugin is included as well). - populate `./src/` with what we _hope/desire_ to bundle.   - `./src/monaco_loader.ts` takes care of loading the lsp workers of monaco, and provides a convenient way to access the main features of the monca-editor via es6-exports.   - `./src/index.ts` contains the editor logic that the web-dev will want to show up in their web-app.     it also imports the `./src/style.css` file, so that it can be bundled along with the rest of the code, without explicitly having to copy it over ourselves.   - `./src/index.html` provides the html wrapper for loading the bundled code (`./index.js` and `./index.css`).     this html file will need to be one of our esbuild bundling configuration's entry-points, so that it can be copied over to the distribution folder.

---
+++ monaco_esbuild4/src/index.ts
@@ -0,0 +1,45 @@
+import { monacoEditor, monacoEnvironment, monacoLanguages } from "./monaco_loader.ts"
+
+import "./style.css"
+
+
+monacoLanguages.typescript.typescriptDefaults.setCompilerOptions({
+	...monacoLanguages.typescript.typescriptDefaults.getCompilerOptions(),
+	target: monacoLanguages.typescript.ScriptTarget.ESNext,
+	module: monacoLanguages.typescript.ModuleKind.ESNext,
+	moduleResolution: monacoLanguages.typescript.ModuleResolutionKind.NodeJs,
+	// noEmit: true,
+	// allowJs: true,
+	checkJs: true,
+	isolatedModules: true,
+	moduleDetection: "force",
+	esModuleInterop: true,
+	allowImportingTsExtensions: true,
+	allowNonTsExtensions: true,
+	alwaysStrict: true,
+	baseUrl: "./",
+})
+
+monacoLanguages.typescript.typescriptDefaults.setDiagnosticsOptions({
+	...monacoLanguages.typescript.typescriptDefaults.getDiagnosticsOptions(),
+	noSemanticValidation: true,
+	noSyntaxValidation: false,
+})
+
+export const editor = monacoEditor.create(document.getElementById("monaco-editor")!, {
+	value: [
+		`import { joinPaths } from "https://jsr.io/@oazmi/kitchensink/0.9.7/src/pathman.ts"`,
+		`import _ from "https://cdn.jsdelivr.net/npm/lodash-es/lodash.js"`,
+		"",
+		`console.log(_.camelCase("Hello World"))`,
+		"const a = 55",
+		"const b: string = `SHEESH-KEBABS`",
+		`joinPaths(b, "snek_kebebs")`,
+		"",
+	].join("\n"),
+	language: "typescript",
+	theme: "vs-dark",
+})
+
+// we assign some variables to the global scope, so that they can be accessed from the dev-console in the browser.
+Object.assign(globalThis, { monacoEditor, monacoEnvironment, monacoLanguages })
